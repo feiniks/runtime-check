f80ed8637739b1e3a37f56f8386f251049d12945
feiniks <36756310+feiniks@users.noreply.github.com>
2020-08-28 16:52:59 +0800

    Create runtime-check.c

---
+++ runtime-check.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void __attribute__((constructor)) check(void){
+    pid_t pid, gpid;
+    pid = getpid();
+    gpid = getpgrp();
+
+    if (pid != gpid) {
+        exit(-1);
+    }
+}
+
+int main(){
+    while(1);
+}
